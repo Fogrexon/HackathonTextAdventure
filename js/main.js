619533c2dea761f4f7e0a7117a51f87be3088a56
fogrexon <ka2445@outlook.jp>
2019-12-08 12:44:40 +0900

    css追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 addEventListener("load", ()=>{
-    let sc = new Scenario("scenario.json");
+    let sc = new Scenario("./src/scenario.json");
     let tv = new TextViewer(document.getElementById("text_container"));
     let bc = new ButtonController(document.getElementById("button1"),document.getElementById("button2"),document.getElementById("button3"));
     let game = new Game(sc, tv, bc);
